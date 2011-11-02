a5b39a6b62b8526fc3e218aba6a1e06d0337bf14
guilin <guileen@gmail.com>
2011-11-02 13:32:10 +0800

    style like tumblr

---
+++ Reference.md
@@ -0,0 +1,19 @@
+References
+====
+
+Node.js
+----
+[express]
+[connect]
+[less]
+[jade]
+[connect-less]
+[mongoskin]
+
+Stylesheets
+----
+[Bootstrap]
+
+JQuery plugins
+----
+[autoresize](http://james.padolsey.com/javascript/jquery-plugin-autoresize/)
--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,16 @@ module.exports = function(app){
       });
   });
 
+  app.get('/user/signup', function(req, res){
+      res.render('users/signup', {
+          title: 'Sign up',
+      });
+  });
+
+  app.get('/user/signin', function(req, res){
+      res.render('users/signin', {
+          title: 'Sign in',
+      });
+  });
+
 }
