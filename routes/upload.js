a41c65eed87d42b6c39dcc451ff9a79bc209106f
Lin Gui <guileen@gmail.com>
2011-11-18 11:19:38 +0800

    test of upload

--- routes/upload.js
+++ routes/upload.js
@@ -1,5 +1,17 @@
+var fs = require('fs')
+  , querystring = require('querystring')
+  , path = require('path')
+  , uploadDir = path.normalize(__dirname + '/../public/upload/');
+
+fs.mkdir(uploadDir, '777');
+
 exports.route = function(app) {
+
   app.post('/upload', function(req, res, next){
-      res.send({success: true});
+      console.dir(req.query);
+      req.pipe(fs.createWriteStream(uploadDir + req.session.user.md5 + '-' + req.query.qqfile));
+      req.on('end', function(){
+          res.json({success: true});
+      });
   });
 }
