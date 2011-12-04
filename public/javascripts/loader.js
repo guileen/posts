3d7b68b0d7b3c494303ef74e208a6e54f8e1c997
Lin Gui <guileen@gmail.com>
2011-12-04 21:20:45 +0800

    bookmark loader.js

---
+++ public/javascripts/loader.js
@@ -0,0 +1,9 @@
+function(window, document, namespace){
+  if(window[namespace] && typeof window[namespace].postToPostsli === 'function'){
+    return;
+  }
+  var script = document.createElement('script');
+  script.setAttribute('charset', 'utf-8');
+  script.src = 'http://posts.li/bookmark.js?' + Math.floor(new Date() / 1E7);
+  document.body.appendChild(script);
+}
