fdfe9070d2b5b4d9767819a17e9bbe9bf7ea4d24
Lin Gui <guileen@gmail.com>
2011-12-08 01:30:47 +0800

    enhance remove UEX

--- public/javascripts/api.js
+++ public/javascripts/api.js
@@ -1,13 +1,34 @@
 // TODO ajaxSetup global
-function removePost(id, el) {
-  if (confirm('remove post?')) {
-    $.post( '/post/' + id + '/remove', null, function(r) {
-        if (r.success) {
-          $(el).data('twipsy').$tip.remove();
-          $(el).parents('.entry').slideUp(function() {
-              $(this).remove();
-          });
+function removePost(id) {
+  $.post( '/post/' + id + '/remove', null, function(r) {
+      if (r.success) {
+        $('#rm-' + id).data('twipsy').$tip.remove();
+        $('#rm-' + id).parents('.entry').slideUp(function() {
+            $(this).remove();
+        });
+      }
+  });
+}
+
+$(function(){
+
+    $('.icon.trash').popover({
+        trigger:'manual'
+      , html:true
+      , placement : 'below'
+      , content : function(){
+          var id = this.id.substring(3);
+          var hideSnip = "$('#rm-" + id + "').popover('hide')";
+          var removeSnip = "removePost('" + id + "');";
+          return '<p class="clearfix">Are you sure?</p><br>'
+          + '<p style="text-align:center;">'
+          + '<a class="btn" href="javascript:void(0)" onclick="' + removeSnip + hideSnip + '">Yes</a>'
+          + '&nbsp;<a class="btn primary" onclick="' + hideSnip + '">No</a></p>'
         }
     });
-  }
-}
+
+    $('.icon.trash').click(function(){
+        $(this).popover('show');
+    })
+
+})
